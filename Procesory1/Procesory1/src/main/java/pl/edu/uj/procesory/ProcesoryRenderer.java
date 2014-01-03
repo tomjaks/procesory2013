6736b3760747cfb58e60273c96830b996fe21dee
tomjaks <tomjaks@gmail.com>
2014-01-03 16:42:07 +0100

    procesory 1

---
+++ Procesory1/Procesory1/src/main/java/pl/edu/uj/procesory/ProcesoryRenderer.java
@@ -0,0 +1,42 @@
+package pl.edu.uj.procesory;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by mmasiuk on 22.12.13.
+ */
+public class ProcesoryRenderer implements GLSurfaceView.Renderer {
+
+    public static int loadShader(int type, String shaderCode){
+
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        int shader = GLES20.glCreateShader(type);
+
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+
+        return shader;
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+        GLES20.glClearColor(0.0f,0.5f,0.0f,0.0f);
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int width, int hight) {
+        GLES20.glViewport(0,0,width,hight);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+    }
+}
